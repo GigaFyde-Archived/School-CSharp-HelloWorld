d7ce9211dec441a19d62dc148912e69b94309e01
Laurant Marijnissen <laurant@gigafyde.dev>
2021-02-04 00:02:14 +0100

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.WriteLine("Wat is uw naam?");
+            string username = Console.ReadLine();
+            Console.WriteLine("Welkom in Developer Land " + username + "!");
+            Console.WriteLine("Het attractiepark voor ontwikkelaars over de hele wereld.");
+            Console.ReadKey();
+        }
+    }
+}
